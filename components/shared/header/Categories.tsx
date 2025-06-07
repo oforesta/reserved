c83a07900b8d9b9b7aa6247bcdd69817a0ef2094
oforesta <nurislamovaolesia@gmail.com>
2025-06-07 23:23:42 +0200

    Changed Women button routing to redirect to homepage

--- components/shared/header/Categories.tsx
+++ components/shared/header/Categories.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Categories = () => {
   return (
     <div className='flex items-center gap-4'>
-      <Link href="/women">Women</Link>
+      <Link href="/">Women</Link>
       <Link href="/men">Men</Link>
       <Link href="/kids">Kids</Link>
     </div>
