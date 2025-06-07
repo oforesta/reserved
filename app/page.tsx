4b420c15d1fc72597c0486c66bb21cead5412e4b
oforesta <nurislamovaolesia@gmail.com>
2025-06-07 21:31:09 +0200

    NextAuth

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="flex-grow">
+    <div className="flex-grow min-h-screen">
       <h1>main page</h1>
     </div>
   );
--- components/shared/header/Categories.tsx
+++ components/shared/header/Categories.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Categories = () => {
   return (
     <div className='flex items-center gap-4'>
-      <Link href="/men">Men</Link>
       <Link href="/women">Women</Link>
+      <Link href="/men">Men</Link>
       <Link href="/kids">Kids</Link>
     </div>
   );
